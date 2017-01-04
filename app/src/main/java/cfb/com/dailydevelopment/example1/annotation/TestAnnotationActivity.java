4c2c081e46179b44dcf3eb2af3ff053993728d10
caofengbin <1050430934@qq.com>
2017-01-04 20:40:51 +0800

    update file git ignore

--- app/src/main/java/cfb/com/dailydevelopment/example1/annotation/TestAnnotationActivity.java
+++ app/src/main/java/cfb/com/dailydevelopment/example1/annotation/TestAnnotationActivity.java
@@ -1,9 +1,6 @@
 package cfb.com.dailydevelopment.example1.annotation;
 
 import android.os.Bundle;
-import android.support.annotation.IntDef;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +17,7 @@ public class TestAnnotationActivity extends AppCompatActivity {
     @OnClick(R.id.testButton)
     private void onClick(View view) {
         mButton.setText("我是click后的文字内容");
-        Toast.makeText(this,"按钮被点击了",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "按钮被点击了", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -30,28 +27,4 @@ public class TestAnnotationActivity extends AppCompatActivity {
         mButton.setText("我是click前的Button内容");
     }
 
-    @NonNull
-    private String testAnnotationMethod(@Nullable String inputString) {
-        return inputString == null ? "MyString" : inputString;
-    }
-
-    public static final int FIRST_NUMBER = 1;
-    public static final int SECOND_NUMBER = 2;
-    public static final int THIRD_NUMBER = 3;
-
-    @IntDef({FIRST_NUMBER,SECOND_NUMBER,THIRD_NUMBER})
-    public @interface Flavour {
-
-    }
-
-    private int myValue;
-
-    @Flavour
-    public int getFlavour() {
-        return myValue;
-    }
-
-    public void setFlavour(@Flavour int value) {
-        this.myValue = value;
-    }
 }
